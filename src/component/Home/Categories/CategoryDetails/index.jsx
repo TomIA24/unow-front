959835962076b5d9d2e2d7afe2e698df462922f8
TomIA24 <169178002+TomIA24@users.noreply.github.com>
2024-08-06 15:53:12 +0200

    amelioration_courses_categs

--- src/component/Home/Categories/CategoryDetails/index.jsx
+++ src/component/Home/Categories/CategoryDetails/index.jsx
@@ -1,39 +1,100 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import CourseItem from "./CourseItem";
 import Nav from "../../../Nav";
-import { Button } from "react-bootstrap";
-
+import { Button, Col, Row,Container } from "react-bootstrap";
+import "./styles.modules.css"
 const CategoryDetails=(props)=>{
     useEffect(()=>{
 
     },[])
-  const refHome=useRef()
+  const [loadMoreOnline,setLoadMoreOnline]=useState(false) 
+  const [loadMoreOffline,setLoadMoreOffline]=useState(false)
+  const [indexItems,setIndexItems]=useState(6)
+  
+  const [indexItemsOffline,setIndexItemsOffline]=useState(6)
+  useEffect(()=>{
+    if(loadMoreOnline && !loadMoreOffline){
+      setIndexItemsOffline(3)
+    }
+    else     if(!loadMoreOnline && loadMoreOffline){
+      setIndexItems(3)
+
+    }
+  },[loadMoreOnline,loadMoreOffline])
+  const updatItemsFunction=()=>{
+    setIndexItems((prev)=>prev+3)
+  }
+  const updatItemsOfflineFunction=()=>{
+    setIndexItemsOffline((prev)=>prev+3)
+  }
+  const groupIntoRows = (items, itemsPerRow) => {
+    const rows = [];
+    for (let i = 0; i < items.length; i += itemsPerRow) {
+      rows.push(items.slice(i, i + itemsPerRow));
+    }
+    return rows;
+  };  const refHome=useRef()
+  const arrayOnline=groupIntoRows(props.onlineCourses,3)
+  const arrayOffline=groupIntoRows(props.offlineCourses,3)
+   
     return (
-        <div>
+        <div >
         <Nav ref={refHome} />
-    <div style={{justifyContent:"center",alignItems:"center",marginTop:"20px"}}>
-        <b style={{marginLeft:"10%",fontSize:"60px",marginBottom:"30px"}}>ONLINE COURSES</b>
-        <hr style={{border:"solid 3px #CD6214",width:"5%",marginLeft:"10%",fontWeight:"30px",height:"20%",borderRadius:"20px"}}/>
-         <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}> {props?.onlineCourses?.map((course)=>(
-            <CourseItem course={course} />
-
-          ))}
+    <div className="d-flex justify-content-center align-items-center mt-4" style={{marginLeft:"3%",marginRight:"3%"}} >
+      <br />
+    <div className="features">
+            ONLINE COURSES
+            <p className="underline"></p>
           </div>
-          <Button style={{display:"flex",justifyContent:"center",alignItems:"center"}}>Other Courses</Button>
-          <div style={{justifyContent:"center",alignItems:"center",marginTop:"20px"}}>
-
-        <b style={{marginLeft:"10%",fontSize:"60px",marginTop:"30px"}}>OFFLINE COURSES</b>
-        <hr  style={{border:"solid 3px #CD6214",width:"5%",marginLeft:"10%",fontWeight:"30px",height:"20%",borderRadius:"20px"}}/>
-        <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-        {props?.offlineCourses?.map((course)=>(
-            
+         <Container className="container-grid">
+          {arrayOnline.slice(0,indexItems/3).map((itemnested)=>(
+            <div>
+          <Row className="row1">
+            {itemnested.map((course)=>(
+            <Col xs={12} md={4}  className="col">
             <CourseItem course={course} />
-
-          ))}
+            </Col>
+            ))
+}
+          
+          </Row>
+          <br />
           </div>
+))}
+          <Row>         
+             <button className="btn_style btn-primary" onClick={()=>{setLoadMoreOnline(true) ;updatItemsFunction()}} ><p className="text_btn">Other Courses</p></button>
+          </Row>
+          </Container>
+          
+          <div style={{justifyContent:"center",alignItems:"center",marginTop:"70px"}}>
+
+          <div className="features">
+            OFFLINE COURSES
+            <p className="underline"></p>
+          </div>
+          <Container className="container-grid">
+          {arrayOffline.slice(0,indexItemsOffline/3).map((itemnested)=>(
+            <div>
+          <Row className="row1">
+            {itemnested.map((course)=>(
+            <Col xs={12} md={4}  className="col">
+            <CourseItem course={course} />
+            </Col>
+            ))
+}
+          
+          </Row>
+          <br />
+          </div>
+))}
+          <Row>         
+             <button className="btn_style btn-primary" onClick={()=>{setLoadMoreOffline(true) ;updatItemsOfflineFunction()}} ><p className="text_btn">Other Courses</p></button>
+          </Row>
+          </Container>
           </div>
 
     </div>
     </div>)  
+
 }
 export default CategoryDetails;
