8ab23e6308c98b982f136ef360e5e979b31f71b7
hamdizer <zeramdinihamdi1@gmail.com>
2024-08-07 14:57:19 +0100

    ui_amelioration_training_list

--- src/component/Home/Categories/CategoryDetails/CourseItem/index.jsx
+++ src/component/Home/Categories/CategoryDetails/CourseItem/index.jsx
@@ -1,35 +1,121 @@
-import styles from "../../../../Main/styles.module.css"
-const CourseItem=(props)=>{
-   
-  return(
-    <div className={styles.topTrainingElements} style={{paddingRight:"7%",marginTop:"3%"}}>
- 
-        <div className={styles.inner_carousel} >
-          {props.course.thumbnail ? (
-            <div className={styles.image}>
-              <img src={props.course.thumbnail} alt={""} className={styles.imagefeatures} />
-
+import {
+  AddTwoTone,
+  PlusOneOutlined,
+  StarOutline,
+  StarRounded,
+} from "@mui/icons-material";
+import styles from "../../../../Main/styles.module.css";
+import stylesItem from "./styles.modules.css";
+const CourseItem = (props) => {
+  return (
+    <div
+      className={styles.topTrainingElements}
+      style={{ paddingRight: "7%", marginTop: "3%" }}
+    >
+      <div className={styles.inner_carousel} style={{ width: "100%" }}>
+        {props.course.thumbnail ? (
+          <div
+            className={styles.image}
+            style={{ marginBottom: "0px !important" }}
+          >
+            <img
+              src={props.course.thumbnail}
+              alt={""}
+              className={styles.imagefeatures}
+            />
+          </div>
+        ) : (
+          <div className={styles.image}>
+            <img
+              src="default-image.png"
+              alt="Default"
+              className={styles.imagefeatures}
+            />
+          </div>
+        )}
+        <div
+          style={{
+            width: "100%",
+            margin: "1px 3px 0px 3px",
+            display: "inline-block",
+          }}
+        >
+          <div className={styles.categorie} style={{ alignItems: "center" }}>
+            <div className={styles.categorietype} style={{ fontSize: "15px" }}>
+              {props.course.subcat}
             </div>
-          ) : (
-            <div className={styles.image}>
-              <img src="default-image.png" alt="Default" className={styles.imagefeatures} /></div>
-          )}
-          <div>
-            <div className={styles.categorie}>
-              <div className={styles.categorietype}>{props.course.subcat}</div>
-              <div className={styles.categoriprice}>{props.course.price} </div>
+            <div
+              className={styles.categoriprice}
+              style={{
+                fontSize: "15px",
+                margin: "0px 15px 0px 0px !important",
+              }}
+            >
+              {props.course.price}{" "}
             </div>
-            <div className={styles.categoriniveau} style={{marginTop:"20px"}}>{props.course.level}</div>
-            <div className={styles.categoridomain} style={{marginTop:"20px"}}>{props.course.title}</div>
-            
+          </div>
+          <div style={{ display: "inline-block", float: "left" }}>
+            <div
+              className={styles.categoriniveau}
+              style={{ marginTop: "8px", fontSize: "15px" }}
+            >
+              {props.course.level}
+            </div>
+            <div
+              className={styles.categoridomain}
+              style={{ marginTop: "8px", fontSize: "15px" }}
+            >
+              {props.course.title}
+            </div>
+          </div>
+          <br />
+          <div
+            style={{
+              display: "inline-flex",
+              float: "left",
+              justifyContent: "space-between",
+              width: "100%",
+              alignItems: "center",
+            }}
+          >
+            <div
+              className={styles.categoriniveau}
+              style={{
+                marginTop: "20px",
+                float: "left",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <StarRounded style={{ color: "yellow" }} />5 (750)
+            </div>
+            <div
+              className={styles.categoridomain}
+              style={{
+                marginTop: "20px",
+                float: "right",
+                margin: "0px 15px 0px 0px !important",
+              }}
+            >
+              {props.course.title}
+            </div>
+          </div>
+          <br />
+          <div
+            style={{
+              display: "flex",
+              float: "right",
+              backgroundColor: "#cd6214",
+              borderRadius: "20px 0px 0px",
+              cursor: "pointer",
+              padding: "2%",
+            }}
+          >
+            <AddTwoTone fontSize="xs" />
           </div>
         </div>
-      
-    <div>
-    
+      </div>
     </div>
-  </div>
-
-  )
-}
-export default CourseItem
+  );
+};
+export default CourseItem;
--- src/component/Home/Categories/CategoryDetails/index.jsx
+++ src/component/Home/Categories/CategoryDetails/index.jsx
@@ -1,100 +1,116 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react";
 import CourseItem from "./CourseItem";
 import Nav from "../../../Nav";
-import { Button, Col, Row,Container } from "react-bootstrap";
-import "./styles.modules.css"
-const CategoryDetails=(props)=>{
-    useEffect(()=>{
+import { Button, Col, Row, Container } from "react-bootstrap";
+import "./styles.modules.css";
+const CategoryDetails = (props) => {
+  useEffect(() => {}, []);
+  const [loadMoreOnline, setLoadMoreOnline] = useState(false);
+  const [loadMoreOffline, setLoadMoreOffline] = useState(false);
+  const [indexItems, setIndexItems] = useState(6);
 
-    },[])
-  const [loadMoreOnline,setLoadMoreOnline]=useState(false) 
-  const [loadMoreOffline,setLoadMoreOffline]=useState(false)
-  const [indexItems,setIndexItems]=useState(6)
-  
-  const [indexItemsOffline,setIndexItemsOffline]=useState(6)
-  useEffect(()=>{
-    if(loadMoreOnline && !loadMoreOffline){
-      setIndexItemsOffline(3)
+  const [indexItemsOffline, setIndexItemsOffline] = useState(6);
+  useEffect(() => {
+    if (loadMoreOnline && !loadMoreOffline) {
+      setIndexItemsOffline(3);
+    } else if (!loadMoreOnline && loadMoreOffline) {
+      setIndexItems(3);
     }
-    else     if(!loadMoreOnline && loadMoreOffline){
-      setIndexItems(3)
-
-    }
-  },[loadMoreOnline,loadMoreOffline])
-  const updatItemsFunction=()=>{
-    setIndexItems((prev)=>prev+3)
-  }
-  const updatItemsOfflineFunction=()=>{
-    setIndexItemsOffline((prev)=>prev+3)
-  }
+  }, [loadMoreOnline, loadMoreOffline]);
+  const updatItemsFunction = () => {
+    setIndexItems((prev) => prev + 3);
+  };
+  const updatItemsOfflineFunction = () => {
+    setIndexItemsOffline((prev) => prev + 3);
+  };
   const groupIntoRows = (items, itemsPerRow) => {
     const rows = [];
     for (let i = 0; i < items.length; i += itemsPerRow) {
       rows.push(items.slice(i, i + itemsPerRow));
     }
     return rows;
-  };  const refHome=useRef()
-  const arrayOnline=groupIntoRows(props.onlineCourses,3)
-  const arrayOffline=groupIntoRows(props.offlineCourses,3)
-   
-    return (
-        <div >
-        <Nav ref={refHome} />
-    <div className="d-flex justify-content-center align-items-center mt-4" style={{marginLeft:"3%",marginRight:"3%"}} >
-      <br />
-    <div className="features">
-            ONLINE COURSES
-            <p className="underline"></p>
-          </div>
-         <Container className="container-grid">
-          {arrayOnline.slice(0,indexItems/3).map((itemnested)=>(
-            <div>
-          <Row className="row1">
-            {itemnested.map((course)=>(
-            <Col xs={12} md={4}  className="col">
-            <CourseItem course={course} />
-            </Col>
-            ))
-}
-          
-          </Row>
-          <br />
-          </div>
-))}
-          <Row>         
-             <button className="btn_style btn-primary" onClick={()=>{setLoadMoreOnline(true) ;updatItemsFunction()}} ><p className="text_btn">Other Courses</p></button>
-          </Row>
-          </Container>
-          
-          <div style={{justifyContent:"center",alignItems:"center",marginTop:"70px"}}>
+  };
+  const refHome = useRef();
+  const arrayOnline = groupIntoRows(props.onlineCourses, 3);
+  const arrayOffline = groupIntoRows(props.offlineCourses, 3);
 
+  return (
+    <div>
+      <Nav ref={refHome} />
+      <div
+        className="d-flex justify-content-center align-items-center mt-4"
+        style={{ marginLeft: "8%", marginRight: "8%" }}
+      >
+        <br />
+        <div className="features">
+          ONLINE COURSES
+          <p className="underline"></p>
+        </div>
+        <Container className="container-grid">
+          {arrayOnline.slice(0, indexItems / 3).map((itemnested) => (
+            <div>
+              <Row className="row1">
+                {itemnested.map((course) => (
+                  <Col xs={12} md={4} className="col">
+                    <CourseItem course={course} />
+                  </Col>
+                ))}
+              </Row>
+              <br />
+            </div>
+          ))}
+          <Row>
+            <button
+              className="btn_style btn-primary"
+              onClick={() => {
+                setLoadMoreOnline(true);
+                updatItemsFunction();
+              }}
+            >
+              <p className="text_btn">Other Courses</p>
+            </button>
+          </Row>
+        </Container>
+
+        <div
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "70px",
+          }}
+        >
           <div className="features">
             OFFLINE COURSES
             <p className="underline"></p>
           </div>
           <Container className="container-grid">
-          {arrayOffline.slice(0,indexItemsOffline/3).map((itemnested)=>(
-            <div>
-          <Row className="row1">
-            {itemnested.map((course)=>(
-            <Col xs={12} md={4}  className="col">
-            <CourseItem course={course} />
-            </Col>
-            ))
-}
-          
-          </Row>
-          <br />
-          </div>
-))}
-          <Row>         
-             <button className="btn_style btn-primary" onClick={()=>{setLoadMoreOffline(true) ;updatItemsOfflineFunction()}} ><p className="text_btn">Other Courses</p></button>
-          </Row>
+            {arrayOffline.slice(0, indexItemsOffline / 3).map((itemnested) => (
+              <div>
+                <Row className="row1">
+                  {itemnested.map((course) => (
+                    <Col xs={12} md={4} className="col">
+                      <CourseItem course={course} />
+                    </Col>
+                  ))}
+                </Row>
+                <br />
+              </div>
+            ))}
+            <Row>
+              <button
+                className="btn_style btn-primary"
+                onClick={() => {
+                  setLoadMoreOffline(true);
+                  updatItemsOfflineFunction();
+                }}
+              >
+                <p className="text_btn">Other Courses</p>
+              </button>
+            </Row>
           </Container>
-          </div>
-
+        </div>
+      </div>
     </div>
-    </div>)  
-
-}
+  );
+};
 export default CategoryDetails;
