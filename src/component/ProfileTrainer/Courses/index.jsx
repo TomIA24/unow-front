0254306c12c915db2c400231ccee1f2b62970f05
Nesrine1333 <nesrine.ncibi@polytechnicien.tn>
2024-10-11 10:17:59 +0100

    add the empty box & fix the modal out of the view

--- src/component/ProfileTrainer/Courses/index.jsx
+++ src/component/ProfileTrainer/Courses/index.jsx
@@ -4,6 +4,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import SendIcon from "@mui/icons-material/Send";
 import React from "react";
 import { request } from "../../../core/api/request";
+import EmptyBox from "../../../shared/components/EmptyBox";
 import Loading from "../../Loading";
 import styles from "./styles.module.css";
 import useCourses from "./useCourses";
@@ -22,54 +23,61 @@ const Courses = () => {
       {loading && <Loading h="30vh" />}
 
       {!loading &&
-        courses.map((course) => (
-          <div key={course._id} className={styles.coursesCard}>
-            <div className={styles.imgContainer}>
-              <img
-                src={process.env.REACT_APP_API + course.Thumbnail?.filePath}
-                alt=""
-                className={styles.img}
-              />
-            </div>
+        (courses.length === 0 ? (
+          <EmptyBox h="30vh" text="You don't have any courses yet" />
+        ) : (
+          courses.map((course) => (
+            <div key={course._id} className={styles.coursesCard}>
+              <div className={styles.imgContainer}>
+                <img
+                  src={process.env.REACT_APP_API + course.Thumbnail?.filePath}
+                  alt=""
+                  className={styles.img}
+                />
+              </div>
 
-            <div className={styles.info}>
-              <div className={styles.group}>
-                <p className={styles.title}>{course.Category}</p>
-                <div className={styles.icons}>
-                  <div
-                    onClick={() => handleDelete(course._id)}
-                    className={styles.icon}
-                  >
-                    <DeleteOutlineIcon
-                      sx={{ color: "var(--primary-color)", fontSize: "16px" }}
-                    />
-                  </div>
-                  <div
-                    onClick={() => navigate(`/course/edit/${course._id}`)}
-                    aria-disabled={course.Status !== "proccessing"}
-                    className={styles.icon}
-                  >
-                    <EditIcon
-                      sx={{ color: "var(--secondary-color)", fontSize: "16px" }}
-                    />
+              <div className={styles.info}>
+                <div className={styles.group}>
+                  <p className={styles.title}>{course.Category}</p>
+                  <div className={styles.icons}>
+                    <div
+                      onClick={() => handleDelete(course._id)}
+                      className={styles.icon}
+                    >
+                      <DeleteOutlineIcon
+                        sx={{ color: "var(--primary-color)", fontSize: "16px" }}
+                      />
+                    </div>
+                    <div
+                      onClick={() => navigate(`/course/edit/${course._id}`)}
+                      aria-disabled={course.Status !== "proccessing"}
+                      className={styles.icon}
+                    >
+                      <EditIcon
+                        sx={{
+                          color: "var(--secondary-color)",
+                          fontSize: "16px"
+                        }}
+                      />
+                    </div>
                   </div>
                 </div>
+                <div className={styles.textContainer}>
+                  <p>{course.Title}</p>
+                </div>
+                <p
+                  className={styles.status}
+                  style={{ "--status-color": "#34A853" }}
+                >
+                  {course.Status}
+                </p>
+                <button onClick={() => navigate(`/course/${course._id}`)}>
+                  <span>Go To Course</span>
+                  <SendIcon sx={{ color: "white", fontSize: "16px" }} />
+                </button>
               </div>
-              <div className={styles.textContainer}>
-                <p>{course.Title}</p>
-              </div>
-              <p
-                className={styles.status}
-                style={{ "--status-color": "#34A853" }}
-              >
-                {course.Status}
-              </p>
-              <button onClick={() => navigate(`/course/${course._id}`)}>
-                <span>Go To Course</span>
-                <SendIcon sx={{ color: "white", fontSize: "16px" }} />
-              </button>
             </div>
-          </div>
+          ))
         ))}
     </div>
   );
